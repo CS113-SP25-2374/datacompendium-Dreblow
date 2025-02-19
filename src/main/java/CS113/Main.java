cb8d2900981795e86065dc255f974d8abf58091b
Derek Dreblow <derek.dreblow@gmail.com>
2025-02-18 20:10:31 -0800

    Not complete yet

--- src/main/java/CS113/Main.java
+++ src/main/java/CS113/Main.java
@@ -1,7 +1,25 @@
 package CS113;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ArrayList<Integer> goArray = new ArrayList<Integer>();
+        ArrayListDAD<Integer> testArray = new ArrayListDAD<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            testArray.add(i);
+        }
+
+        goArray.add(4, null);
+        testArray.add(4, null);
+
+        System.out.println(testArray);
+        System.out.println(testArray.size());
+
+        System.out.println(goArray);
+
     }
 }
